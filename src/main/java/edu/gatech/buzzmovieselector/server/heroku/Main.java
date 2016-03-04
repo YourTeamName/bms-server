b030d16eb46ac2f0664eef0b44d08d3fd41cc69e
Jianing Yang <jed970610@gmail.com>
2016-03-03 21:51:35 -0500

    data

--- src/main/java/edu/gatech/buzzmovieselector/server/heroku/Main.java
+++ src/main/java/edu/gatech/buzzmovieselector/server/heroku/Main.java
@@ -1,56 +1,63 @@
 package edu.gatech.buzzmovieselector.server.heroku;
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.webapp.WebAppContext;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
 
 /**
- * This class launches the web application in an embedded Jetty container. This is the entry point to your application. The Java
+ * This class launches the web application in an embedded Jetty container.
+ * This is the entry point to your application. The Java
  * command that is used for launching should fire this main method.
  */
 public class Main {
 
-    private static URI getBaseURI(String hostname, int port) {
-        return UriBuilder.fromUri("http://0.0.0.0/").port(port).build();
+
+    // Base URI the Grizzly HTTP server will listen on
+    public static final String BASE_URI = "http://localhost:80/";
+
+    /**
+     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this
+     * application.
+     *
+     * @return Grizzly HTTP server.
+     */
+    public static HttpServer startServer() {
+        // create a resource config that scans for JAX-RS resources and
+        // providers
+        // in com.example package
+        final ResourceConfig rc = new ResourceConfig().packages("edu.gatech" +
+                ".buzzmovieselector.server");
+
+        // create and start a new instance of grizzly http server
+        // exposing the Jersey application at BASE_URI
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI)
+                , rc);
     }
 
-    protected static HttpServer startServer(URI uri) throws IOException {
-        System.out.println("Starting grizzly...");
-        ResourceConfig rc = new PackagesResourceConfig("com.agile.spirit.openapi");
-        return GrizzlyServerFactory.createHttpServer(uri, rc);
-    }
-
+    /**
+     * Main method.
+     *
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
-        String hostname = System.getenv("HOSTNAME");
-        if (hostname == null) {
-            hostname = "localhost";
-        }
-
-        boolean isOnLocal = false;
-        String port = System.getenv("PORT");
-        if (port == null) {
-            isOnLocal = true;
-            port = "9998";
-        }
-
-        URI uri = getBaseURI(hostname, Integer.valueOf(port));
-
-        HttpServer httpServer = startServer(uri);
-        System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", uri, uri));
-        if (isOnLocal) {
-            System.in.read();
-            httpServer.stop();
-        } else {
-            while (true) {
-                System.in.read();
-            }
-        }
-
+        final HttpServer server = startServer();
+        System.out.println(String.format("Jersey app started with WADL " +
+                "available at "
+                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+//        System.in.read();
+//        server.stop();
     }
+
+}
 
     /*
     public static void main(String[] args) throws Exception{
-        // The port that we should run on can be set into an environment variable
+        // The port that we should run on can be set into an environment
+        variable
         // Look for that variable and default to 8080 if it isn't there.
         String webPort = System.getenv("PORT");
         if (webPort == null || webPort.isEmpty()) {
@@ -63,9 +70,12 @@ public class Main {
         root.setContextPath("/");
         // Parent loader priority is a class loader setting that Jetty accepts.
         // By default Jetty will behave like most web containers in that it will
-        // allow your application to replace non-server libraries that are part of the
-        // container. Setting parent loader priority to true changes this behavior.
-        // Read more here: http://wiki.eclipse.org/Jetty/Reference/Jetty_Classloading
+        // allow your application to replace non-server libraries that are
+        part of the
+        // container. Setting parent loader priority to true changes this
+        behavior.
+        // Read more here: http://wiki.eclipse
+        .org/Jetty/Reference/Jetty_Classloading
         root.setParentLoaderPriority(true);
 
         final String webappDirLocation = "src/main/webapp/";
@@ -77,4 +87,4 @@ public class Main {
         server.start();
         server.join();
     }*/
-}
+
