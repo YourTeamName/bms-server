eed824db6d17851db8b83ee72549fb91d757dfc5
Jianing Yang <jed970610@gmail.com>
2016-03-09 14:45:20 -0500

    data

---
+++ macsetup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# abort script if something fails
+set -e
+
+if [ ! $(which brew) ]; then
+    echo "-- Installing homebrew - a package manager for OS X"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+echo "-- Updating homebrew package list"
+brew update
+
+if [[ ! $(brew ls --versions brew-cask) ]]; then
+    echo "-- Installing homebrew cask"
+    brew install caskroom/cask/brew-cask
+fi
+
+#  needed for the app
+echo "-- Installing maven through homebrew"
+brew install maven
+
+echo "-- Installing heroku-toolbelt through homebrew"
+brew install heroku-toolbelt
+
+# set environment variables
+echo "-- Setting heroku evironment variables"
+heroku config:get DATABASE_URL -s  >> .env
